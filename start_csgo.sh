c751531d4c4430dde0e33db3fa50a95801500e33
David Monschein <dmonschein@linode.com>
2017-03-08 11:34:18 -0500

    updated configs, reduced cruft

--- start_csgo.sh
+++ start_csgo.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/steam/Steam/csgo-ds
-/home/steam/Steam/csgo-ds/srcds_run -game csgo -usercon -maxplayers_override 24 -steam_dir /home/steam/Steam -steamcmd_script /home/steam/Steam/csgo-script -autoupdate +game_type 0 +game_mode 1 +mapgroup mg_active +map de_dust2 +sv_setsteamaccount 52CBF721BB6BF7F6F5B801849D7E64C7 -net_port_try 1
+/home/steam/Steam/csgo-ds/srcds_run -game csgo -usercon -maxplayers_override 24 -steam_dir /home/steam/Steam -steamcmd_script /home/steam/Steam/csgo-script -tickrate 128 -autoupdate +game_type 0 +game_mode 0 +mapgroup mg_reserves +map cs_office +sv_setsteamaccount $TOKEN -net_port_try 1
